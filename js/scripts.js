ba65604fccba2d5e447835ed37dc085f04dc634f
unknown <swm1000@hotmail.com>
2018-02-09 22:52:02 -0500

    init jquery vars

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+var $overlay = $("<div id = 'overlay'></div>");
+
+$("body").append($overlay);
+
+var $image = $("<img>");
+
+$overlay.append($image);
+
+var $button = $("<img id = 'xbutton'>");
+
+$overlay.append($button);
