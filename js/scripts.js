b8d37b8c7da246e04fadcfebffabc8a252b36dbe
unknown <swm1000@hotmail.com>
2018-02-10 19:40:28 -0500

    attach imgs to vars

--- js/scripts.js
+++ js/scripts.js
@@ -13,4 +13,8 @@ $overlay.append($button);
 
 $("#gallery a").click(function(event) {
     event.preventDefault();
+
+    var imgsrc = $(this).attr("href");
+    $image.attr("src", imgsrc)
+    $button.attr("src", "https://image.flaticon.com/icons/svg/69/69381.svg")
 });
