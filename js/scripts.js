ee5817f512d87b76aeb33caa60793a58a7d8d61e
unknown <swm1000@hotmail.com>
2018-02-10 19:49:24 -0500

    fix imgsrc tag/attr

--- js/scripts.js
+++ js/scripts.js
@@ -14,7 +14,7 @@ $overlay.append($button);
 $("#gallery a").click(function(event) {
     event.preventDefault();
 
-    var imgsrc = $(this).attr("href");
+    var imgsrc = $("#gallery img").attr("src");
     $image.attr("src", imgsrc);
     $button.attr("src", "https://res.cloudinary.com/teepublic/image/private/s--21aFqFwq--/t_Preview/b_rgb:191919,c_limit,f_jpg,h_630,q_90,w_630/v1466431125/production/designs/554244_1.jpg");
 
