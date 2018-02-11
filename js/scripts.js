d8cb366ad8eedf4cde8a4f4dd4123052abc24b8d
unknown <swm1000@hotmail.com>
2018-02-10 20:01:51 -0500

    fix overlay img src

--- js/scripts.js
+++ js/scripts.js
@@ -6,15 +6,15 @@ var $image = $("<img>");
 
 $overlay.append($image);
 
-var $button = $("<img id = 'xbutton'>");
+var $button = $("<img id = 'closebutton'>");
 
 $overlay.append($button);
 
 
-$("#gallery a").click(function(event) {
+$("#gallery a img").click(function(event) {
     event.preventDefault();
 
-    var imgsrc = $("#gallery img").attr("src");
+    var imgsrc = $(this).attr("src");
     $image.attr("src", imgsrc);
     $button.attr("src", "https://res.cloudinary.com/teepublic/image/private/s--21aFqFwq--/t_Preview/b_rgb:191919,c_limit,f_jpg,h_630,q_90,w_630/v1466431125/production/designs/554244_1.jpg");
 
